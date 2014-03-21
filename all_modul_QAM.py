0c2e0caa3ad4d36c940abce5aa8ae05d30285228
Oxtay <okhtay@gmail.com>
2014-03-20 17:51:03 -0700

    further simplified the code, added a test signal

--- all_modul_QAM.py
+++ all_modul_QAM.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+
 # Comparison of clustering with and without the use of k-Center clustering
 # Written by: Okhtay Azarmanesh
 # Penn State University
@@ -6,47 +9,32 @@ from KCenter import KCenter
 import numpy as np
 import scipy
 import matplotlib
+import csv
 
-n = 256    # Number of samples
+n   = 256  # Number of samples
 SNR = 5    # The amount of noise after passing from an AWGN channel
-
+K   = 16   # Testing 16-QAM as an example
 # Using K-center for initializing
-for con in range(1,2):
-    constel = 2*con
-    K = 2^constel
-    x = randintvec(n,K)        # Create a signal source for PSK.
-    h = modem.qammod('M',K,'PhaseOffset',0,'SymbolOrder','binary')
-    y = modulate(h,x)          # Modulate the signal x.
-    y_chann = awgn(y,SNR)      # Output of AWGN channel
-    g = modem.qamdemod(h)      
-    # Create a demodulator object from a modem.qammod object
-    # and display its properties.
-    z = demodulate(g,y_chann)  # Demodulate the signal y_chann. 
-    
-    # The k-mean command should be performed on y_chann
-    Data = [real(y_chann), imag(y_chann)]
-    
-    [cluster,u]=KCenter(Data,K)
-    [IDX1, C1] = kmeans2(Data, K, 'Start', u)
-    
+##################
+#
+#    Modulator
+#
+##################
+ 
+# The k-mean command should be performed on y_chann
+Data        = [real(y_chann), imag(y_chann)]   
+[cluster,u] = KCenter(Data,K)
+[IDX1, C1]  = kmeans2(Data, K, 'Start', u)
+  
 # Using k observations at random
-for con in range(1,2):
-    constel = 2*con
-    K = 2^constel
-    x = randintvec(n,K)        # Create a signal source for PSK.
-    h = modem.qammod('M',K,'PhaseOffset',0,'SymbolOrder','binary')
-    y = modulate(h,x)          # Modulate the signal x.
+##################
+#
+#    Modulator
+#
+##################
 
-    y_chann = awgn(y,SNR)      # Output of AWGN channel
-    g = modem.qamdemod(h)      
-    # Create a demodulator object from a modem.qammod object
-    # and display its properties.
-    z = demodulate(g,y_chann)  # Demodulate the signal y_chann. 
-
-
-    # The k-mean command should be performed on y_chann
-    Data = [real(y_chann), imag(y_chann)]
-
-    [IDX1, C1] = kmeans2(Data, K, 'Start', 'uniform')
+Data        = [real(y_chann), imag(y_chann)]
+[cluster,u] = KCenter(Data,K)
+[IDX1, C1]  = kmeans2(Data, K, 'Start', 'uniform')
 
     
