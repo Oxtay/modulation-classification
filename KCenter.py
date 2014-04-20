b5eef294bc5990d61269a34b8176099acc26b30c
Okhtay Azarmanesh <okhtay@gmail.com>
2014-04-19 17:01:58 -0700

    KCenter is almost complete. There are two items in todo list

--- KCenter.py
+++ KCenter.py
@@ -1,3 +1,4 @@
+import random
 def KCenter(Data,K):
     # k-center clustering method
     # Written by: Okhtay Azarmanesh
@@ -11,24 +12,27 @@ def KCenter(Data,K):
     # This is a greedy approximation algorithm of metric k-center optimization problem and it achieves an approximation factor of 2 in k iterations.
 
     # randomly select an object as the initial point
-    Init = ceil(rand(1)*size(Data,1))
-    h = zeros(K,size(Data,2))
-    h(1,:) = Data(Init,:)
+    # To Do List
+    #   Conversion of functions is not complete
+    #   Try each function separately and make sure it works
+    Init = ceil(random.uniform(1)*Data.shape[0])
+    h = zeros(K,Data.shape[1])
+    h[1,:] = Data(Init,:)
 
 
-    dist = sum( (Data-repmat(h(1,:),[size(Data,1),1])).^2,2 )
-    cluster = ones(1,size(Data,1))
+    Euclid_dist = sum( (Data-repmat(h(1,:),[Data.shape[0],1])).^2,2 )
+    cluster = ones(1,Data.shape[0])
 
-    dist1=dist
+    Euclid_dist1=Euclid_dist
     Hind=Init
     for i in range(2,K):
-        dist1(Hind)=-1
-        D = max(dist1)
-        ind = find(dist==D)
+        Euclid_dist1(Hind)=-1
+        D = max(Euclid_dist1)
+        ind = find(Euclid_dist==D)
         Hind=[Hind,ind(1)]
         h(i,:) = Data(ind(1),:)
-        distnew = sum( (Data-repmat(h(i,:),[size(Data,1),1])).^2,2 )
-        cluster(find(distnew-dist<0))=i
+        Euclid_distnew = sum( (Data-repmat(h(i,:),[Data.shape[0],1])).^2,2 )
+        cluster(find(Euclid_distnew-dist<0))=i
         dist(find(distnew-dist<0))=distnew(find(distnew-dist<0))
         dist1=dist
         
